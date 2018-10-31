9c479bab291a161bcfbfe7c3069e9fc0855e1b8f
wan-zhen <kelly811124811124@hotmail.com>
2018-10-31 14:53:48 +0800

    練習 computed methods watch & lifeCycle & 表達式

---
+++ src/components/Demo.vue
@@ -0,0 +1,94 @@
+<template>
+  <div>
+    <input v-model="msg">
+    <p>{{msg}}</p>
+    <p>{{reversedMessageComputed}}</p>
+    <p>{{reversedMessageMethods()}}</p>
+    
+    <p v-html="html"></p>
+    <p v-if="seen">看的到ㄏㄏ看不到ㄎㄎ</p>
+    <p v-else>看的到ㄎㄎ看不到ㄏㄏ</p>
+    <button :disabled="false" @click="changeData">Button</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Demo',
+  data: function() {
+    return {
+      msg: 'demo',
+      html: '<span style="color:red;">red</span>',
+      seen: false
+    };
+  },
+  // 响应式依赖 计算属性是基于它们的依赖进行缓存的 (cache)
+  // 依賴的值沒有改變 放在這裡的屬性不會走進來
+  // ex : this.msg 有被改變才會走進來 如果想在畫面上一直走進來 除非改動 msg 的值，
+  //      不然就改用 method 綁在畫面上吧
+  computed: {
+    // 计算属性的 getter
+    reversedMessageComputed: function() {
+      console.log('computed');
+      // `this` 指向实例
+      return this.msg
+        .split('')
+        .reverse()
+        .join('');
+    }
+  },
+  // 跟 computed 不同的點只有在於 只要有呼叫到都會進來
+  // 不像 computed 有 cache 依賴的值就不會再走進來
+  methods: {
+    changeData: function() {
+      this.seen = !this.seen;
+      this.msg = '你好';
+    },
+    // 也可直接綁在表達式上
+    reversedMessageMethods: function() {
+      console.log('methods');
+      // `this` 指向实例
+      return this.msg
+        .split('')
+        .reverse()
+        .join('');
+    }
+  },
+  // computed 加強版 当需要在数据变化时执行异步或开销较大的操作时，这个方式是最有用的
+  watch: {
+    // 如果 `msg` 发生改变，这个函数就会运行
+    msg: function(newQuestion, oldQuestion) {
+      console.log(newQuestion, oldQuestion);
+    }
+  },
+  beforeCreate: function() {
+    console.warn('beforeCreate', this.msg);
+  },
+  created: function() {
+    console.warn('created', this.msg);
+  },
+  beforeMount: function() {
+    console.warn('beforeMount');
+  },
+  mounted: function() {
+    console.warn('mouted');
+  },
+  beforeUpdate: function() {
+    // 有值被改變
+    console.warn('beforeUpdate');
+  },
+  updated: function() {
+    console.warn('updated');
+  },
+  beforeDestroy: function() {
+    console.warn('beforeDestroy');
+  },
+  destroyed: function() {
+    console.warn('destroyed');
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
---
+++ src/views/About.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <demo></demo>
+    <HelloWorld/>
+    <hello-world msg="test"></hello-world>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Demo from '@/components/Demo.vue';
+import HelloWorld from '@/components/HelloWorld.vue';
+
+export default {
+  name: 'about',
+  components: {
+    Demo,
+    HelloWorld
+  }
+};
+</script>
